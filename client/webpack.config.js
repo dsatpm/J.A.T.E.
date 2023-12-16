8ffd916a67e82a21f7703814d1db4f830d33ee5b
dsatpm <jeremiahjh@gmail.com>
2023-12-15 22:08:03 -0800

    manifest, sw manifest, loaders configured

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,24 +7,71 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+	return {
+		mode: 'development',
+		entry: {
+			main: './src/js/index.js',
+			install: './src/js/install.js',
+		},
+		output: {
+			filename: '[name].bundle.js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+		plugins: [
+      // html template
+			new HtmlWebpackPlugin({
+				template: './index.html',
+				title: 'Dreamcatcher Text Editor',
+			}),
+			// service worker manifest
+			new InjectManifest({
+				swSrc: './src-sw.js',
+				swDest: 'src-sw.js',
+			}),
+			// manifest file, json format
+			new WebpackPwaManifest({
+				fingerprints: false,
+				inject: true,
+				name: 'Dreamcatcher Text Editor',
+				short_name: 'D.T.E',
+				description: 'Keep track of important tasks!',
+				background_color: '#0F111A',
+				theme_color: '#0F111A',
+				start_url: './',
+				publicPath: './',
+				icons: [
+					{
+						src: path.resolve('src/images/logo.png'),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join('assets', 'icons'),
+					},
+				],
+			}),
+		],
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+		module: {
+			// style and CSS loaders
+			rules: [
+				{
+					test: /\.css$/i,
+					use: ['style-loader', 'css-loader'],
+				},
+				// babel loader
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+							plugins: [
+								'@babel/plugin-proposal-object-rest-spread',
+								'@babel/transform-runtime',
+							],
+						},
+					},
+				},
+			],
+		},
+	};
 };
